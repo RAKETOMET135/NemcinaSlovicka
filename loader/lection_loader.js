a970509dd20edec3403c6c16fc633a0e2c276e54
RAKETOMET135 <simonuvmail@gmail.com>
2026-02-12 18:47:03 +0100

    added words for lection 17 selectio 1

--- loader/lection_loader.js
+++ loader/lection_loader.js
@@ -214,6 +214,7 @@ export function loadVerbFile(webRootPath, fileName, onLoadFunction){
 
 export function getVerbFileNames(){
     const verbFileNameList = [
+        ["slovesa_lekce17.json", "Lekce 17 - výběr 1"],
         ["slovesa_lekce16.json", "Lekce 16 - výběr 1"],
         ["slovesa_lekce16_str86-90.json", "Lekce 16 - str 86-90"],
         ["slovesa_lekce15_2.json", "Lekce 15 - výběr 2"],
@@ -228,6 +229,7 @@ export function getVerbFileNames(){
 
 export function getLectionFileNames(){
     const lectionNameList = [
+        ["lekce17vyber1.json", "Lekce 17 - výběr 1"],
         ["lekce16vyber1.json", "Lekce 16 - výběr 1"],
         ["lekce16_str86-90.json", "Lekce 16 - str 86-90"],
         ["lekce15vyber2.json", "Lekce 15 - výběr 2"],
